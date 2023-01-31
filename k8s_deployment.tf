c6c61d2248bd7128263c8210408dffc8a7e6f8b9
henryrestive <henry@restivetech.com.au>
2023-02-01 10:58:30 +1100

    Initial commit

---
+++ k8s_deployment.tf
@@ -0,0 +1,42 @@
+resource "kubernetes_deployment" "myapp" {
+  metadata {
+    name = "myapp-frontend-dev"
+    labels = {
+      app = "myapp"
+      component = "frontend"
+      environment = "dev"
+    }
+  }
+
+  spec {
+    replicas = "1"
+
+    selector {
+      match_labels = {
+        app = "myapp"
+        component = "frontend"
+        environment = "dev"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "myapp"
+          component = "frontend"
+          environment = "dev"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "myapp-frontend-dev"
+          # ports {
+          #   containerPort = 80
+          # }
+        }
+      }
+    }
+  }
+}
